10772ee4a69a3ea4de1ee87165c23e2a522818af
eshvar289 <eshvar89@hotmail.com>
2020-08-01 23:00:39 +0530

    added the method which return the full name & and used to apply the salary raise

--- oops.py
+++ oops.py
@@ -1,5 +1,6 @@
 # Python OOPS
 class Employee:
+    raise_amount = 1.05
 
     def __init__(self, first_name, last_ame, email, age, salary):
         self.first_name = first_name
@@ -7,3 +8,9 @@ class Employee:
         self.age = age
         self.salary = salary
         self.email = email
+
+    def full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def apply_raise(self):
+        self.salary = int(self.salary * self.raise_amount)
