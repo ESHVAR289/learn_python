c56a5e86cca59ff5eee63baafb88e4c382bc1c42
eshvar289 <eshvar89@hotmail.com>
2020-08-01 23:31:37 +0530

    created the methods

--- Company.py
+++ Company.py
@@ -1,2 +1,15 @@
 class Company:
-    pass
+
+    def __init__(self, cmp_name, cmp_establishment_date, cmp_ceo):
+        self.cmp_name = cmp_name
+        self.cmp_establishment_date = cmp_establishment_date
+        self.cmp_ceo = cmp_ceo
+
+    def get_cmp_ceo(self):
+        return self.cmp_ceo
+
+    def get_cmp_name(self):
+        return self.cmp_name
+
+    def get_cmp_establishment_date(self):
+        return self.cmp_establishment_date
