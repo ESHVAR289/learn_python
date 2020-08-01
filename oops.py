9e05fc7b6a2127bfa643b08eefcf578c847217de
eshvar289 <eshvar89@hotmail.com>
2020-08-01 22:47:08 +0530

    created the employee class with first name, last name,age and salary as class instance variable

---
+++ oops.py
@@ -0,0 +1,9 @@
+# Python OOPS
+class Employee:
+
+    def __init__(self, first_name, last_ame, age, salary):
+        self.first_name = first_name
+        self.last_name = last_ame
+        self.age = age
+        self.salary = salary
+
